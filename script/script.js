2aead2f23c046e6ca132d944381469ca181959fb
lydie debaecker <lydie37250@gmail.com>
2025-12-16 14:44:22 +0100

    Correction des bugs et optimisation du code

--- script/script.js
+++ script/script.js
@@ -1,20 +1,21 @@
 // Function to add the "navbarDark" class to the navbar on scroll
 function handleNavbarScroll() {
     const header = document.querySelector(".navbar");
-    window.onscroll = function () {
-        const top = window.scrollY;
-        if (top >= 100) {
-            header.classList.add("navbarDark");
-        } else {
-            header.classList.remove("navbarDark");
-        }
-    };
+window.addEventListener("scroll", () => {
+    const top = window.scrollY;
+    if (top >= 100) {
+        header.classList.add("navbarDark");
+    } else {
+        header.classList.remove("navbarDark");
+    }
+});
 }
+
 
 // Function to handle navbar collapse on small devices after a click
 function handleNavbarCollapse() {
-    const navLinks = document.querySelectorAll(".nav-item");
-    const menuToggle = document.getElementById("navbarSupportedContent");
+    const navLinks = document.querySelectorAll(".nav-link");
+    const menuToggle = document.getElementById("navbarSupportedContent"); 
 
     navLinks.forEach((link) => {
         link.addEventListener("click", () => {
@@ -29,28 +30,23 @@ function createSkillsFromJSON() {
     let row = document.createElement("div");
     row.classList.add("row");
 
-    // Load the JSON file
     fetch("data/skills.json")
         .then((response) => response.json())
         .then((data) => {
-            // Iterate through the JSON data and create HTML elements
             data.forEach((item, index) => {
                 const card = document.createElement("div");
                 card.classList.add("col-lg-4", "mt-4");
                 card.innerHTML = `
                     <div class="card skillsText">
                         <div class="card-body">
-                            <img src="./images/${item.image}" />
+                            <img src="./images/${item.image}" alt="${item.title}" loading="lazy" />
                             <h4 class="card-title mt-3">${item.title}</h4>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
                 `;
-
-                // Append the card to the current row
                 row.appendChild(card);
 
-                // If the index is a multiple of 3 or it's the last element, create a new row
                 if ((index + 1) % 3 === 0 || index === data.length - 1) {
                     container.appendChild(row);
                     row = document.createElement("div");
@@ -59,37 +55,33 @@ function createSkillsFromJSON() {
             });
         });
 }
+
 // Function to dynamically create HTML elements from the JSON file
 function createPortfolioFromJSON() {
     const container = document.querySelector("#portfolio .container");
     let row = document.createElement("div");
     row.classList.add("row");
 
-    // Load the JSON file
     fetch("data/portfolio.json")
         .then((response) => response.json())
         .then((data) => {
-            // Iterate through the JSON data and create HTML elements
             data.forEach((item, index) => {
                 const card = document.createElement("div");
                 card.classList.add("col-lg-4", "mt-4");
                 card.innerHTML = `
                     <div class="card portfolioContent">
-                    <img class="card-img-top" src="images/${item.image}" style="width:100%">
-                    <div class="card-body">
-                        <h4 class="card-title">${item.title}</h4>
-                        <p class="card-text">${item.text}</p>
-                        <div class="text-center">
-                            <a href="${item.link}" class="btn btn-success">Lien</a>
+                        <img class="card-img-top" src="images/${item.image}" alt="${item.title}" loading="lazy" style="width:100%">
+                        <div class="card-body">
+                            <h4 class="card-title">${item.title}</h4>
+                            <p class="card-text">${item.text}</p>
+                            <div class="text-center">
+                                <a href="${item.link}" class="btn btn-success">Lien</a>
+                            </div>
                         </div>
                     </div>
-                </div>
                 `;
+                row.appendChild(card);
 
-                // Append the card to the current row
-                row.apendChild(card);
-
-                // If the index is a multiple of 3 or it's the last element, create a new row
                 if ((index + 1) % 3 === 0 || index === data.length - 1) {
                     container.appendChild(row);
                     row = document.createElement("div");
@@ -98,6 +90,7 @@ function createPortfolioFromJSON() {
             });
         });
 }
+
 
 // Call the functions to execute the code
 handleNavbarScroll();
