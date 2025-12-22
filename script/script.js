f953724c938d28255f2b6aa7d88abe2c22a11488
lydie debaecker <lydie37250@gmail.com>
2025-12-22 10:47:17 +0100

    Finalisation du travail sur le responsive

--- script/script.js
+++ script/script.js
@@ -1,42 +1,41 @@
 // Function to add the "navbarDark" class to the navbar on scroll
 function handleNavbarScroll() {
-    const header = document.querySelector(".navbar");
-window.addEventListener("scroll", () => {
+  const header = document.querySelector(".navbar");
+  window.addEventListener("scroll", () => {
     const top = window.scrollY;
     if (top >= 100) {
-        header.classList.add("navbarDark");
+      header.classList.add("navbarDark");
     } else {
-        header.classList.remove("navbarDark");
+      header.classList.remove("navbarDark");
     }
-});
+  });
 }
-
 
 // Function to handle navbar collapse on small devices after a click
 function handleNavbarCollapse() {
-    const navLinks = document.querySelectorAll(".nav-link");
-    const menuToggle = document.getElementById("navbarSupportedContent"); 
+  const navLinks = document.querySelectorAll(".nav-link");
+  const menuToggle = document.getElementById("navbarSupportedContent");
 
-    navLinks.forEach((link) => {
-        link.addEventListener("click", () => {
-            new bootstrap.Collapse(menuToggle).toggle();
-        });
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      new bootstrap.Collapse(menuToggle).toggle();
     });
+  });
 }
 
 // Function to dynamically create HTML elements from the JSON file
 function createSkillsFromJSON() {
-    const container = document.querySelector("#skills .container");
-    let row = document.createElement("div");
-    row.classList.add("row");
+  const container = document.querySelector("#skills .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
 
-    fetch("data/skills.json")
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((item, index) => {
-                const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                card.innerHTML = `
+  fetch("data/skills.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        card.innerHTML = `
                     <div class="card skillsText">
                         <div class="card-body">
                             <img src="./images/${item.image}" alt="${item.title}" loading="lazy" />
@@ -45,52 +44,51 @@ function createSkillsFromJSON() {
                         </div>
                     </div>
                 `;
-                row.appendChild(card);
+        row.appendChild(card);
 
-                if ((index + 1) % 3 === 0 || index === data.length - 1) {
-                    container.appendChild(row);
-                    row = document.createElement("div");
-                    row.classList.add("row");
-                }
-            });
-        });
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
 }
 
 // Function to dynamically create HTML elements from the JSON file
 function createPortfolioFromJSON() {
-    const container = document.querySelector("#portfolio .container");
-    let row = document.createElement("div");
-    row.classList.add("row");
+  const container = document.querySelector("#portfolio .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
 
-    fetch("data/portfolio.json")
-        .then((response) => response.json())
-        .then((data) => {
-            data.forEach((item, index) => {
-                const card = document.createElement("div");
-                card.classList.add("col-lg-4", "mt-4");
-                card.innerHTML = `
+  fetch("data/portfolio.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        card.innerHTML = `
                     <div class="card portfolioContent">
                         <img class="card-img-top" src="images/${item.image}" alt="${item.title}" loading="lazy" style="width:100%">
                         <div class="card-body">
                             <h3 class="card-title">${item.title}</h3>
                             <p class="card-text">${item.text}</p>
                             <div class="text-center">
-                                <a href="${item.link}" class="btn btn-success">Lien</a>
+                                <a href="${item.link}" class="btn btn-success" target="_blank">Lien</a>
                             </div>
                         </div>
                     </div>
                 `;
-                row.appendChild(card);
+        row.appendChild(card);
 
-                if ((index + 1) % 3 === 0 || index === data.length - 1) {
-                    container.appendChild(row);
-                    row = document.createElement("div");
-                    row.classList.add("row");
-                }
-            });
-        });
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
 }
-
 
 // Call the functions to execute the code
 handleNavbarScroll();
